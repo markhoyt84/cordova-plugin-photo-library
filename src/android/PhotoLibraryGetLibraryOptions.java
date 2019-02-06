4a8183b41cea103773d89f610862343024a2f5c0
Mark Hoyt <mark.hoyt970@gmail.com>
2019-02-06 07:47:35 -0700

    All package contents being uploaded

---
+++ src/android/PhotoLibraryGetLibraryOptions.java
@@ -0,0 +1,17 @@
+package com.terikon.cordova.photolibrary;
+
+public class PhotoLibraryGetLibraryOptions {
+
+  public final int itemsInChunk;
+  public final double chunkTimeSec;
+  public final boolean includeAlbumData;
+  public final boolean includeVideos;
+
+  public PhotoLibraryGetLibraryOptions(int itemsInChunk, double chunkTimeSec, boolean includeAlbumData, boolean includeVideos) {
+    this.itemsInChunk = itemsInChunk;
+    this.chunkTimeSec = chunkTimeSec;
+    this.includeAlbumData = includeAlbumData;
+    this.includeVideos = includeVideos;
+  }
+
+}
